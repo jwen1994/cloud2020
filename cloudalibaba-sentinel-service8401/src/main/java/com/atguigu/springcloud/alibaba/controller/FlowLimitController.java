39fa9a2c8ff0610ec69bccd4a57b9d41c0aa09aa
jwen <526464869@qq.com>
2021-03-04 20:16:54 +0800

    修改FlowLimitController

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,9 +1,13 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
+@Slf4j
 public class FlowLimitController {
     @GetMapping("/testA")
     public String testA() {
@@ -12,7 +16,31 @@ public class FlowLimitController {
 
     @GetMapping("/testB")
     public String testB() {
-
         return "------testB";
+    }
+
+    /*@GetMapping("/testD")
+    public String testD() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("testD 测试RT");
+        return "------testD";
+    }*/
+
+    @GetMapping("/testD")
+    public String testD() {
+        log.info("testD 测试RT");
+        int age = 10 / 0;
+        return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE() {
+        log.info("testE 测试异常数");
+        int age = 10 / 0;
+        return "------testE 测试异常数";
     }
 }
